cc184b702d6710fed7662bf8f6f6f877d193cb00
Ingvild <ingvild_kongsgaard@hotmail.com>
2017-01-20 15:46:04 -0800

    Following along the tutorial and implementing the stuff

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+	render() {
+		return (
+			<div>
+				<br/>
+				<br/>
+				<p>NotFound</p>
+			</div>
+			)
+		}
+	}
+
+export default NotFound;
