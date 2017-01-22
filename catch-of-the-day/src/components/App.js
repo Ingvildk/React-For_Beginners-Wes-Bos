0593e0a94d7f5ece49570da14d758b538e565fde
Ingvild <ingvild_kongsgaard@hotmail.com>
2017-01-22 15:56:08 -0800

    Passing event handlers thorugh props. Building the state hierarchy

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,47 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+		//initial state
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		//update our state
+		const fishes = {...this.state.fishes};
+		//add in our new fish
+		const timestamp = Date.now();
+		fishes['fish-${timestamp}'] = fish;
+		//set state
+		this.setState({fishes});
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
+					<ul className="list-of-fishes">
+						<li></li>
+					</ul>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} 
+					loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Fish extends React.Component {
+	render() {
+		<li className="menu-fish">Fish</li>
+	}
+
+}
+
+export default Fish;
