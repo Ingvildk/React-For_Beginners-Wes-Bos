8a78c0f985816093d8605e392f29d1881eae1b7e
Ingvild <ingvild_kongsgaard@hotmail.com>
2017-01-23 15:42:17 -0800

    Fixed the error

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 class Fish extends React.Component {
 	render() {
-		const details = this.props;
+		const {details} = this.props;
 		return (
 		<li className="menu-fish">
 			<img src={details.image} alt={details.name} />
-			{this.props.details.name}
+			<h3>
+				{details.name}
+				<span className="price">{details.price}</span>
+			</h3>	
 		</li>
 		)
 	}
