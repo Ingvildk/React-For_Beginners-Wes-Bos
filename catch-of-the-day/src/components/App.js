86f0cb3c536e1afd33ebfd6f63de469cdc7556cd
Ingvild <ingvild_kongsgaard@hotmail.com>
2017-01-24 12:34:36 -0800

    passing down functions and states to child components

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		//initial state
 		this.state = {
 			fishes: {},
@@ -32,6 +33,15 @@ class App extends React.Component {
 			fishes: sampleFishes
 		})
 	}
+
+	addToOrder(key) {
+		// take a copy of our state
+		const order = {...this.state.order};
+		// update or add the new number of fish ordered
+		order[key] = order[key] + 1 || 1;
+		//update our state
+		this.setState({ order });
+	}
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -40,7 +50,8 @@ class App extends React.Component {
 					<ul className="list-of-fishes">
 						{Object
 							.keys(this.state.fishes)
-							.map(key => <Fish key={key} details={this.state.fishes[key]}/> )}
+							.map(key => <Fish key={key} index={key} details={this.state.fishes[key]}
+									addToOrder={this.addToOrder}/> )}
 					</ul>
 				</div>
 				<Order />
